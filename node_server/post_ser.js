547b69ac72c90f00de32eb1a3feb6cb1edf7b48c
pillarBoy <601991736@qq.com>
2017-03-14 16:59:35 +0800

    allow post json and string data

--- node_server/post_ser.js
+++ node_server/post_ser.js
@@ -4,16 +4,16 @@ var bodyParser = require('body-parser')
 
 // 解决本地调试 跨域请求
 server.all('*', function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "X-Requested-With");
-res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-res.header("X-Powered-By",' 3.2.1');
-res.header("Content-Type", "application/json;charset=utf-8");
-next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1');
+  res.header("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+  next();
 });
 
-
-server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.json()); // 支持json 格式
+server.use(bodyParser.urlencoded({ extended: true})) // 支持默认 x-www-form-urlencoded
 
 var port = process.env.PORT || 8081
 var router = express.Router()
@@ -27,15 +27,17 @@ router.get('/get/:page/:name', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  console.log(req.get('Content-Type'))
+  console.log(req.query)
+  console.log(req.body)
   if(req.body.accesstoken === '30589745-03b5-4a53-85c7-df327fe17c48') {
-    res.json({code: '200', data: [1,2,3,4,5], msg: 'welcome, Alice login success.'})
+    res.status(200).json({code: '200', data: [1,2,3,4,5], msg: 'welcome, Alice login success.', success: true})
   } else {
-    // res.status(401).send('accesstoken error!');
-    res.status(500);
-    // res.render('error', { error: err })
     res.json({code: '1001', data: [], msg: 'sorry, you need to use to get pars.'})
   }
 })
+
+
 router.post('/wklist', (req, res) => {
   var {page, projectCode} = req.body
   if (projectCode === 'T0001') {
